426b120645ad1dd7bce931c9cb091c6109e9e3f5
Gershy Lev <gbay9855@yahoo.com>
2015-07-22 17:54:51 -0700

    Add and set up Parse

--- ReadySetPoker/AppDelegate.swift
+++ ReadySetPoker/AppDelegate.swift
@@ -7,6 +7,8 @@
 //
 
 import UIKit
+import Parse
+import Bolts
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
@@ -15,6 +17,18 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
 
     func application(application: UIApplication, didFinishLaunchingWithOptions launchOptions: [NSObject: AnyObject]?) -> Bool {
+        // [Optional] Power your app with Local Datastore. For more info, go to
+        // https://parse.com/docs/ios_guide#localdatastore/iOS
+        Parse.enableLocalDatastore()
+        
+        // Initialize Parse.
+        Parse.setApplicationId("evBPAta2oDPYkyo9nWjyE3lsEy745R6JUCnDjyzn",
+            clientKey: "WRL99ZE53pTzlTDPAwUFiAYSEIziDOazZ1rgOfpD")
+        
+        // [Optional] Track statistics around application opens.
+        PFAnalytics.trackAppOpenedWithLaunchOptions(launchOptions)
+        
+        // ...
         // Override point for customization after application launch.
         return true
     }
