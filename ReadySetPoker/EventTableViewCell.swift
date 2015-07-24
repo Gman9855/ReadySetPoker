7c72c77164b0494b8fb5e269bec40f9e90a8a7eb
Gershy Lev <gbay9855@yahoo.com>
2015-07-24 15:16:26 -0700

    Add ParseUI framework and EventTableViewCell class

---
+++ ReadySetPoker/EventTableViewCell.swift
@@ -0,0 +1,13 @@
+//
+//  EventTableViewCell.swift
+//  ReadySetPoker
+//
+//  Created by Gershy Lev on 7/24/15.
+//  Copyright (c) 2015 ReadySetPoker. All rights reserved.
+//
+
+import Cocoa
+
+class EventTableViewCell: PFTableViewCell {
+
+}
