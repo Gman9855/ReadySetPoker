e88ac9d3d68d8565b806d45e9dcc06ef744a7aba
Gershy Lev <gbay9855@yahoo.com>
2015-07-29 17:03:38 -0700

    Add InviteFriendsVC

---
+++ ReadySetPoker/InviteFriendsViewController.swift
@@ -0,0 +1,13 @@
+//
+//  InviteFriendsViewController.swift
+//  ReadySetPoker
+//
+//  Created by Gershy Lev on 7/28/15.
+//  Copyright (c) 2015 ReadySetPoker. All rights reserved.
+//
+
+import Cocoa
+
+class InviteFriendsViewController: UITableViewController {
+
+}
