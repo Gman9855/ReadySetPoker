e0b4b0dfdfeb8dcd95d3bd013cb401c792ed0e6b
Gershy Lev <gbay9855@yahoo.com>
2015-08-13 18:10:59 -0700

    Implement RSVP VC with ActionSheetPicker

--- ReadySetPoker/RSVPViewController.swift
+++ ReadySetPoker/RSVPViewController.swift
@@ -6,8 +6,27 @@
 //  Copyright (c) 2015 ReadySetPoker. All rights reserved.
 //
 
-import Cocoa
+import UIKit
+import ActionSheetPicker_3_0
 
 class RSVPViewController: UITableViewController {
 
+    @IBOutlet weak var guestsLabel: UILabel!
+    
+    override func tableView(tableView: UITableView, didSelectRowAtIndexPath indexPath: NSIndexPath) {
+        let cell = tableView.cellForRowAtIndexPath(indexPath)
+        if cell?.reuseIdentifier == "BringingGuestsCell" {
+            let array = ["0 Guests", "1 Guest", "2 Guests", "3 Guests"]
+            var picker = ActionSheetStringPicker(title: nil, rows: array, initialSelection: 0, doneBlock: { (picker: ActionSheetStringPicker!, selectedIndex: Int, selectedValue: AnyObject!) -> Void in
+                self.guestsLabel.text = selectedValue as? String
+                }, cancelBlock: { (picker: ActionSheetStringPicker!) -> Void in
+                print("Block picker canceled")
+            }, origin: self.view)
+            picker.showActionSheetPicker()
+        }
+    }
+    
+    @IBAction func cancelButtonTapped(sender: UIBarButtonItem) {
+        dismissViewControllerAnimated(true, completion: nil)
+    }
 }
