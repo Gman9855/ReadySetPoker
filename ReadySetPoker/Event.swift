a11d6ade0b0d7331d2898ac445713b46daafbf89
Gershy Lev <gbay9855@yahoo.com>
2015-08-15 14:46:17 -0700

    Add more location related properties to Event PFObject

--- ReadySetPoker/Event.swift
+++ ReadySetPoker/Event.swift
@@ -13,8 +13,20 @@ class Event : PFObject {
     @NSManaged var title: String
     @NSManaged var eventDescription: String
     @NSManaged var date: NSDate
-    @NSManaged var location: String
+    @NSManaged var location: CLLocationCoordinate2D
+    @NSManaged var address: String
+    @NSManaged var streetAddress: String
+    @NSManaged var city: String
+    @NSManaged var state: String
+    @NSManaged var zipCode: String
     @NSManaged var host: PFUser
     @NSManaged var comments: NSArray
     @NSManaged var attendees: NSArray
+    @NSManaged var guestsJoining: Int
+    
+    enum eventStatus: String {
+        case PendingInvite = "Pending Invite",
+             Going = "Going",
+             NotGoing = "Not going"
+    }
 }
