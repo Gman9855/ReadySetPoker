daa982733e6c690142812780a53d4ead17764fc7
Gershy Lev <gbay9855@yahoo.com>
2015-09-25 14:54:41 -0700

    Add logic when updating RSVP status

--- ReadySetPoker/RSVPViewController.swift
+++ ReadySetPoker/RSVPViewController.swift
@@ -50,10 +50,100 @@ class RSVPViewController: UITableViewController {
     @IBAction func rsvpButtonTapped(sender: UIButton) {
         var segmentedControlIndex: Int = segmentedControl.selectedSegmentIndex
         var isGoing = segmentedControlIndex == 0
+        if !isGoing { guestsJoining = 0 }
+        var changedNumberOfGuests = false
         
-        // update model on parse
+        println("Invite before had a \(invite.inviteStatus) invite status with \(invite.numberOfGuests) guests, and \(invite.event.numberOfAttendees) people going with \(invite.event.numberOfSpotsLeft) spots left")
+        var updatedPartiesCount = 0
+        var shouldSave = true
+        if isGoing {
+            if invite.inviteStatus == "Going" {
+                if invite.numberOfGuests != guestsJoining {
+                    changedNumberOfGuests = true
+                    
+                    if guestsJoining > invite.numberOfGuests {
+                        updatedPartiesCount += guestsJoining - invite.numberOfGuests
+                    } else {
+                        updatedPartiesCount -= invite.numberOfGuests - guestsJoining
+                    }
+                } else {
+                    shouldSave = false
+                    self.delegate?.RSVPViewControllerDidUpdateInvite(nil)
+                    self.dismissViewControllerAnimated(true, completion: nil)
+                }
+            } else if invite.inviteStatus == "Not Going" || invite.inviteStatus == "Pending" {
+                updatedPartiesCount += guestsJoining + 1
+            }
+        } else if invite.inviteStatus == "Going" {
+            updatedPartiesCount -= invite.numberOfGuests + 1
+        }
         
-        delegate?.RSVPViewControllerDidRespondToRSVP(isGoing, withGuests: guestsJoining)
-        dismissViewControllerAnimated(true, completion: nil)
+        if invite.event.numberOfSpotsLeft - updatedPartiesCount < 0 {
+            UIAlertView(title: "Error", message: "Not enough seats left!", delegate: self, cancelButtonTitle: "Okay").show()
+            shouldSave = false
+        }
+        
+        if shouldSave {
+            invite.event.incrementKey("numberOfAttendees", byAmount: updatedPartiesCount)
+            invite.event.incrementKey("numberOfSpotsLeft", byAmount: -updatedPartiesCount)
+            invite.inviteStatus = isGoing ? "Going" : "Not Going"
+            invite.numberOfGuests = guestsJoining
+            
+            MBProgressHUD.showHUDAddedTo(self.navigationController?.view, animated: true)
+            invite.saveInBackgroundWithBlock { (succeeded: Bool, error: NSError?) -> Void in
+                if succeeded {
+                    println("Successfully updated invite status")
+                    println("Invite now has a \(self.invite.inviteStatus) invite status with \(self.invite.numberOfGuests) guests, and \(self.invite.event.numberOfAttendees) people going with \(self.invite.event.numberOfSpotsLeft) spots left")
+                    
+                    var userQuery = PFUser.query()!
+                    var invites = self.invite.event.relationForKey("invites")
+                    var invitesQuery = invites.query()
+                    invitesQuery?.whereKey("inviteStatus", notEqualTo: "Not Going")
+                    invitesQuery?.whereKey("invitee", notEqualTo: PFUser.currentUser()!)
+                    invitesQuery?.includeKey("invitee")
+                    invitesQuery?.findObjectsInBackgroundWithBlock({ (invites: [AnyObject]?, error: NSError?) -> Void in
+                        println(invites)
+                        if invites?.count > 0 {
+                            let inviteResults = invites as! [Invite]
+                            let invitees = inviteResults.map({ (invite: Invite) -> PFUser in
+                                return invite.invitee
+                            })
+                            
+                            var pushQuery = PFInstallation.query()!
+                            pushQuery.whereKey("user", containedIn: invitees)
+                            
+                            var push = PFPush()
+                            push.setQuery(pushQuery)
+                            let currentUserName = PFUser.currentUser()!.objectForKey("fullName") as! String
+                            var pushString: String
+                            if changedNumberOfGuests {
+                                pushString = "\(currentUserName) updated the number of guests they're bringing for \(self.invite.event.title)"
+                            } else {
+                                pushString = "\(currentUserName) updated their status to '\(self.invite.inviteStatus)' for \(self.invite.event.title)"
+                            }
+                            let data = ["alert" : pushString, "eventObjectId" : self.invite.event.objectId!]
+                            push.setData(data)
+                            push.sendPushInBackgroundWithBlock({ (success: Bool, error: NSError?) -> Void in
+                                if success {
+                                    println("Pushed notification successfully")
+                                }
+                                if error != nil {
+                                    println("Failed to push notification")
+                                }
+                            })
+                        }
+                    })
+                    self.delegate?.RSVPViewControllerDidUpdateInvite(self.invite)
+                    self.dismissViewControllerAnimated(true, completion: nil)
+                } else {
+                    println(error)
+                    UIAlertView(title: "Error", message: "Could not save RSVP.  Please check your connection and try again.", delegate: self, cancelButtonTitle: "Okay").show()
+                    // display error alert to user
+                }
+                dispatch_async(dispatch_get_main_queue(), { () -> Void in
+                    MBProgressHUD.hideHUDForView(self.navigationController?.view, animated: true)
+                })
+            }
+        }
     }
 }
